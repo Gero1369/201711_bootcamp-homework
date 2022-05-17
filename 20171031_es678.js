e8d57202345b3a981de52060f41055ce7bb096c1
Geoffrey Pearson <g._pearson@rocketmail.com>
2022-05-17 10:39:02 -0500

    Add files via upload

---
+++ 20171031_es678.js
@@ -0,0 +1,3 @@
+let info = words => `This ${words[0]} is created in the new ${words[1]} with ${words[2]}!`
+
+console.log(info(['function','"backtick syntax"','no parenthesis around the variable, no curly brackets, and no concatenation']))
